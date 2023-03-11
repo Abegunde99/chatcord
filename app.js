1c13ab000ccfbc0efc1947bd8cea8cf32ce1645c
Abegunde99 <preciousolanrewaju1998@gmail.com>
2023-03-11 10:09:04 +0100

    I createdd the file message.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const socketIo = require('socket.io');
+const formatMessages = require('./utils/message');
+
 
 const app = express();
 
@@ -16,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //     res.sendFile('index.html')
 // })
 
-
+const botName = "chatChod"
 
 const server = app.listen(PORT, () => {
     console.log(`server connected successfully with port ${PORT}`);
@@ -24,17 +26,17 @@ const server = app.listen(PORT, () => {
 const io = socketIo(server);
 
 io.on('connection', (socket) => {
-    socket.emit('message', "Welcome to chatCord")
-    socket.broadcast.emit('message', 'A user has joined')
+    socket.emit('message', formatMessages(botName,"Welcome to chatCord"))
+    socket.broadcast.emit('message', formatMessages(botName,'A user has joined'))
 
     //listen for a disconnect event
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat');
+        io.emit('message', formatMessages(botName,'A user has left the chat'));
     })
 
     //listen for the chatMessage
     socket.on('message', (msg) => {
        //emit that message to all clients
-       io.emit('message', msg) 
+       io.emit('message', formatMessages('USER', msg)) 
     })
 })
